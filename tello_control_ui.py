d912dd71f113042c4098d6553d55d16bc3ab55b1
K9oo <70756772+K9oo@users.noreply.github.com>
2020-11-25 12:46:52 +0900

    Update tello_control_ui.py

--- tello_control_ui.py
+++ tello_control_ui.py
@@ -30,7 +30,7 @@ class TelloUI:
         self.stopEvent = None  
         
         # control variables
-        self.distance = 1  # default distance for 'move' cmd ----
+        self.distance = 0.5  # default distance for 'move' cmd ----
         self.degree = 90  # default degree for 'cw' or 'ccw' cmd
         # if the pose recognition mode is opened 
         self.pose_mode = False        
